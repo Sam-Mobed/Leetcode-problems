b9d33a2dc4c1e9602c7386878a9d178bd6ab89c2
Sam-Mobed <masdebom@gmail.com>
2023-06-01 14:30:14 -0400

    finished mergeSortedArray

--- Easy/MergeSortedArray/solution.py
+++ Easy/MergeSortedArray/solution.py
@@ -1,23 +1,4 @@
-#two sorted arrays in increasing order
-#merge the two array
 
-#loop through array1 (by index), for each of its elements check if the corresponding
-#one in arr2 is bigger, if it is, insert it.
-
-#if element in array2 is bigger, we don't increment the counter
-
-
-#is it good practice to modify the argument and return it?
-#don't think so, but here the result should not be returned but instead
-#stored inside arr1, so its fine
-
-#the accomodation where len(arr1)=m+n changes things a lot
-
-#in python(like many other OOP languages, the only mode of parameter
-#passing is call by sharing. this  means thaat the parameters inside the 
-#function become aliases of fthe actual arguments
-#so function can change mutable objects passed as arguments,
-#but can't change their identity
 
 def mergeArrays(nums1, nums2, m, n):
     i=0
@@ -29,17 +10,20 @@ def mergeArrays(nums1, nums2, m, n):
         nums1+=nums2
         #nums1=nums2 can't change the identity of mutable obj inside of a function
         return
-    while (j<n):
-        if (nums1[i]>=nums2[j]):
-            nums1.insert(i, nums2[j]) 
-            j+=1
+    while j<n:
+        while(nums1[i]<nums2[j]):
             i+=1
-            continue
-        i+=1
-        if(i==n):
-            break
+            if (i==len(nums1)):
+                #here we have to clear all the remaining 0's inside
+                #nums1
+                nums1+=nums2 #we've gone through nums1
+                return
+        nums1.insert(i, nums2[j])
+        nums2.pop(j)
+        j+=1
 
 #tests
+#it works but we have to get rid of the 0's at the end of nums1
 if __name__=="__main__":
     test1=[1,2,3,0,0,0]
     nums12=[2,5,6]
