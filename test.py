8b968858ba906f7cf5e9be9fecdc62f7e35900c6
Sam-Mobed <masdebom@gmail.com>
2023-07-17 15:34:04 -0400

    exercises

--- test.py
+++ test.py
@@ -2,36 +2,30 @@
 Just to test stuff.
 """
 
-def canJump(nums) -> bool:
+def evalRPN(tokens) -> int:
+        
+    #the last value that computed
+    operations = {"+", "-", "*", "/"}
+    
+    #at the end there will be a single value in the stack, and it will be the result
+    tokensStack=[]
 
-        skip=0
+    for token in tokens:
+        if token not in operations:
+            tokensStack.append(int(token))
+        else:
+            #the order matters
+            num2=tokensStack.pop()
+            num1=tokensStack.pop()
+            if token=="+":
+                tokensStack.append(num1+num2)
+            elif token=="-":
+                tokensStack.append(num1-num2)
+            elif token=="*":
+                tokensStack.append(num1*num2)
+            else:
+                tokensStack.append(num1//num2)
 
-        for index, jumpVal in enumerate(nums):
-            if skip!=0:
-                skip-=1
-                continue
-            #this is as far as you can go
-            nextIndex = index + jumpVal
+    return tokensStack[0]
 
-            if nextIndex>=len(nums)-1 and nums[-1]-jumpVal<=1:
-                return True
-            
-            #next we find the highest but achievable step that we can reach.
-            maxVal=0
-            maxValIndex=0
-            if nextIndex>len(nums)-1:
-                nextIndex = len(nums)-1
-
-            while nextIndex!=index:
-                if nums[nextIndex]>maxVal and nums[nextIndex]-jumpVal<=1:
-                    maxVal = nums[nextIndex]
-                    maxValIndex = nextIndex
-            
-                nextIndex-=1
-
-            skip = (maxValIndex - index) -1
-            
-        return False
-
-
-canJump([2,3,1,1,4])
+evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"])
