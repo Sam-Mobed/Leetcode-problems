124ad024856f7afa59884600e4b39fcd39cea933
Sam-Mobed <masdebom@gmail.com>
2023-08-08 16:02:05 -0400

    problems

--- test.py
+++ test.py
@@ -2,30 +2,18 @@
 Just to test stuff.
 """
 
-def evalRPN(tokens) -> int:
-        
-    #the last value that computed
-    operations = {"+", "-", "*", "/"}
-    
-    #at the end there will be a single value in the stack, and it will be the result
-    tokensStack=[]
-
-    for token in tokens:
-        if token not in operations:
-            tokensStack.append(int(token))
-        else:
-            #the order matters
-            num2=tokensStack.pop()
-            num1=tokensStack.pop()
-            if token=="+":
-                tokensStack.append(num1+num2)
-            elif token=="-":
-                tokensStack.append(num1-num2)
-            elif token=="*":
-                tokensStack.append(num1*num2)
+def climbStairs(n: int) -> int:
+        table={key: 0 for key in range(1, n+1)} #initialize the table
+        table[1]=1
+        def dp(n,table):
+            if n==0:
+                return 1
+            elif table[n]!=0:
+                return table[n]
             else:
-                tokensStack.append(num1//num2)
+                table[n] += dp(n-1,table) + dp(n-2,table)
+        
+        dp(n,table)
+        return table[n]
 
-    return tokensStack[0]
-
-evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"])
+climbStairs(3)
