7d356aa5e47cb7c92751ba5038dc4b50081a8166
Sam-Mobed <masdebom@gmail.com>
2023-08-17 20:13:48 -0400

    solutions

--- test.py
+++ test.py
@@ -2,25 +2,23 @@
 Just to test stuff.
 """
 
-def combine(n: int, k: int):
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-    res = []
+def combine(inorder, postorder):
 
-    def backtrack(num,currList):
-        if num>n:
-            res.append(currList[:])
-            return
-        if len(currList)==k:
-            res.append(currList[:])
-            backtrack(num,currList[:(k-1)])
-            return
-        backtrack(num+1,currList+[num])
-    
-    for i in range(1,n):
-        backtrack(i,[])
+    if not inorder or not postorder:
+        return None
 
-    if n==1:
-        return [[1]]
-    return res
+    root = TreeNode(postorder[-1])
+    mid = inorder.index(postorder[-1])
+    right = postorder.index(inorder[mid+1])
+    root.left = combine(inorder[:mid], postorder[:right])
+    root.right = combine(inorder[mid+1:], postorder[right:-1])
 
-print(combine(4,2))
+    return root
+
+print(combine([9,3,15,20,7],[9,15,7,20,3]))
