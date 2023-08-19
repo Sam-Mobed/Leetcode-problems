add8fc16d9da94c6639adae15a51cf4c1cd238f0
Sam-Mobed <masdebom@gmail.com>
2023-08-19 12:51:56 -0400

    solutions

--- test.py
+++ test.py
@@ -9,7 +9,28 @@ class TreeNode:
         self.right = right
 
 
-x=['1','2','3']
-print(x)
-print(''.join(x))
-print(int(''.join(x)))
+def hIndex(citations) -> int:
+    d = {}
+
+    h_index = 0
+    for c in citations:
+        if c==0:
+            continue
+
+        if c not in d:
+            d[c]=1
+        else:
+            d[c]+=1
+            if d[c]==c and c>h_index:
+                h_index=c
+
+        for key in d:
+            if key>c:
+                d[key]+=1
+                if d[c]==c and c>h_index:
+                    h_index=c
+
+    return h_index
+
+x=[3,0,6,1,5]
+hIndex(x)
