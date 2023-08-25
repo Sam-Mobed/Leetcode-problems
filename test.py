7f321611858049b78321204b75b60852eeee8827
Sam-Mobed <masdebom@gmail.com>
2023-08-25 13:44:50 -0400

    solutions

--- test.py
+++ test.py
@@ -2,33 +2,19 @@
 Just to test stuff.
 """
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+def palindromeIndex(s):
+    # Write your code here
+    if s == s[::-1]:
+        return -1
+        
+    n = len(s)
+    for i in range(n//2):
+        if s[i]!=s[n-1-i]:
+            if s[i:n-1-i] == s[i:n-1-i][::-1]:
+                return n-1-i
+            elif s[i+1:n-i] == s[i+1:n-i][::-1]:
+                return i
+    return -1
 
 
-def insert(trie,word):
-    for i,c in enumerate(word):
-        if c in trie:
-            if trie[c][1] or i==len(word)-1:
-                return True
-        else:
-            trie[c] = {}, i==len(word)-1
-            
-        trie,_ = trie[c]
-    return False
-
-def noPrefix(words):
-    trie = {}
-    for word in words:
-        if insert(trie,word):
-            print("BAD SET")
-            print(word)
-            return
-    print(trie)
-    print("GOOD SET")
-
-
-noPrefix(['aab', 'defgab', 'abcde', 'aabcde', 'bbbbbbbbbb', 'jabjjjad'])
+palindromeIndex('ababe')
