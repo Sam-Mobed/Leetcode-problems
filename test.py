0f28a48a0fc3774c53912aeb36a30b1571229867
Sam-Mobed <masdebom@gmail.com>
2023-09-11 12:00:42 -0400

    solutions

--- test.py
+++ test.py
@@ -2,6 +2,26 @@
 Just to test stuff.
 """
 
+def reverseVowels(s: str) -> str:
 
-grid = [[0,0,0]*3]
-print(grid)
+    vowels={'a','e','i','o','u','A','E','I','O','U'}
+    s_arr = list(s)
+
+    ptr1, ptr2 = 0, len(s_arr)-1
+
+    while ptr1<ptr2:
+        if s_arr[ptr1] not in vowels:
+            ptr1+=1
+            continue
+        elif s_arr[ptr2] not in vowels:
+            ptr2-=1
+            continue
+        
+        s_arr[ptr1], s_arr[ptr2] = s_arr[ptr2], s_arr[ptr1]
+        ptr1+=1
+        ptr2-=1
+    
+    return ''.join(s_arr)
+
+
+reverseVowels('hello')
