ee61c1ad565369d11aa5016494f9c16605baa69f
Sam-Mobed <masdebom@gmail.com>
2023-09-16 17:19:51 -0400

    solutions

--- test.py
+++ test.py
@@ -2,21 +2,19 @@
 Just to test stuff.
 """
 
-def maxOperations(nums,k) -> int:
+def merge(intervals):
 
-    c = Counter(nums) # Counter({1: 1, 2: 1, 3: 1, 4: 1})
-    res = 0
-    seen = set()
+    intervals.sort(key= lambda i : i[0])
 
-    for x in c:
-        if x not in seen and (k-x) in c:
-            if x==k-x:
-                res += c[x]//2
-            else:
-                res += min(c[x],c[k-x])
-            seen.add(x)
-            seen.add(k-x)
+    new_intervals = [intervals[0]]
 
-    return res
+    for first,last in intervals[1:]:
+        if first<=new_intervals[-1][1]:
+            if new_intervals[-1][1]<last:
+                new_intervals[-1][1]=last
+        else:
+            new_intervals.append([first,last])
 
-maxOperations([3,1,3,4,3],6)
+    return new_intervals
+
+merge([[1,3],[4,8],[1,10]])
